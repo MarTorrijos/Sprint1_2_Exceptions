bdb938181e61f3711ccb46bf20e7b692983750d0
mar <martorrijos@protonmail.com>
2025-04-02 12:26:57 +0200

    Applied round 2 of feedback

--- src/Level_1/controller/runMain.java
+++ src/Level_1/controller/runMain.java
@@ -11,30 +11,33 @@ public class runMain {
         Sale sales = new Sale();
 
         // Will return empty since there are no products in the list
-        try {
-            sales.calculateTotal();
-        } catch (EmptySaleException e) {
-            System.out.println(e.getMessage());
-        }
+        handleEmptySaleException(sales);
 
         sales.getProducts().add(new Product("Pillow", 12));
         sales.getProducts().add(new Product("Blanket", 23));
 
-        // Will return calculateTotal
+        handleEmptySaleException(sales);
+
+        System.out.println("Total price of the sale " + sales.getTotalPrice());
+
+        handleIndexOutOfBoundsException(sales);
+
+    }
+
+    private static void handleEmptySaleException(Sale sales) {
         try {
             sales.calculateTotal();
         } catch (EmptySaleException e) {
             System.out.println(e.getMessage());
         }
+    }
 
-        System.out.println("Total price of the sale " + sales.getTotalPrice());
-
+    private static void handleIndexOutOfBoundsException(Sale sales) {
         try {
             System.out.println(sales.getProducts().get(3));
         } catch (IndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
 }
